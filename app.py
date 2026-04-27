e4c863ea369bfed79630cc47650b3905e0ae6a91
LHCC123 <luciaa.hrndzz@proton.me>
2026-04-27 13:35:37 +0200

    Fix para Railway (puerto dinámico)

--- app.py
+++ app.py
@@ -61,4 +61,6 @@ def delete_user():
     return jsonify({"message": f"Usuario '{username}' eliminado"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
